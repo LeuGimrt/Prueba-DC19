7820e1b9b226f8a4293edde4634ef66d94a4a7ec
LeuGimrt <miguel.rodriguez18@unmsm.edu.pe>
2021-01-03 16:09:20 -0500

    v4

--- app.py
+++ app.py
@@ -30,6 +30,13 @@ def foro():
 def topic():
     return render_template('topic.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
